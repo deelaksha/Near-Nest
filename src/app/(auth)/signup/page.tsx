43b5736073a1bf8137691e1c850f8b2d4b7d9cd4
deelakshadeekshth@gmail.com <deelakshadeekshith@gmail.com>
2025-03-19 09:29:32 +0530

    complete complete authentication work

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from "react";
+import { SignUpForm } from "./components/SignUpForm";
+const SignUpPage=()=>
+{
+    return(
+    <div className="flex h-svh items-center"> <SignUpForm/></div>
+    )
+}
+export default SignUpPage
